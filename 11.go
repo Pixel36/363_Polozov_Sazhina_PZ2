4460a7d53b093bd13a9809c53b6f3ccffe615db7
Pixel <71515743+Pixel36@users.noreply.github.com>
2025-09-06 11:21:14 +0500

    Update 11.go

--- 11.go
+++ 11.go
@@ -1,1 +1,36 @@
+package main
 
+import (
+    "fmt"
+)
+
+type Product struct {
+    Name     string
+    Category string
+    Price    float64
+}
+
+var products = []Product{
+    {"Ноутбук", "Электроника", 15000.0},
+    {"Смартфон", "Электроника", 10000.0},
+    {"Футбоьный мяч", "Спорт", 1000.0},
+    {"Монитор", "Электроника", 12000.0},
+    {"Повербанк", "Электроника", 3000.0},
+}
+
+func filter(products []Product, maxPrice float64, category string) []Product {
+    var filtered []Product
+    for _, p := range products {
+        if p.Price < maxPrice && p.Category == category {
+            filtered = append(filtered, p)
+        }
+    }
+    return filtered
+}
+
+func main() {
+    result := filter(products, 10000.0, "Электроника")
+    for _, p := range result {
+        fmt.Println(p.Name, p.Category, p.Price)
+    }
+}
