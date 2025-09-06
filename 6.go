92bd3eec37ff04971cdb8f8d21dedf8547ef183b
Pixel <71515743+Pixel36@users.noreply.github.com>
2025-09-06 11:01:31 +0500

    Update 6.go

--- 6.go
+++ 6.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+func main(){
+	tags :=[][]string {{"go","backend"}, {"git","go","tools"}}
+	tagsMap :=map[string]bool{}
+	for _,tags := range tags{
+		for _,tag := range tags{
+			tagsMap[tag]=true
+		} 
+	}
+	fmt.Println(tags)
+	fmt.Println(tagsMap)
+}
